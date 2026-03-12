c9e2ee22496132954b36b6d5b3daf2b9b5b8c201
Samilla Soarez <soarezsamilla@gmail.com>
2026-03-12 18:41:44 +0000

    troque esse emoji para: 😟 (_for element span_)

--- src/components/quiz/QuizContainer.tsx
+++ src/components/quiz/QuizContainer.tsx
@@ -46,7 +46,7 @@ export const QuizContainer: React.FC = () => {
       question: "O que você espera de um material que ajude a elaborar um PEI?",
       options: [
         { label: "Praticidade e modelos prontos para usar", icon: "👍" },
-        { label: "Organização e economia de tempo", icon: "😊" }
+        { label: "Organização e economia de tempo", icon: "😟" }
       ]
     },
     {
