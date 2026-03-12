b30ce844d01cc065235e3d339919d54b38f6bb0a
Samilla Soarez <soarezsamilla@gmail.com>
2026-03-12 21:46:40 +0000

    troque somente esse texto e nada alem disso, troque para esse: Um guia e

--- src/components/quiz/QuizContainer.tsx
+++ src/components/quiz/QuizContainer.tsx
@@ -74,7 +74,7 @@ export const QuizContainer: React.FC = () => {
     },
     {
       title: "Plano Educacional PEI 4 e 5 Anos",
-      description: "Estruturas profissionais para uma escrita rápida.",
+      description: "Um guia educacional para alunos de 4 e 5 anos na educação Infantil, histórico escolar, saúde e desenvolvimento do aluno.",
       image: "https://image2url.com/r2/default/images/1773351423137-18ea825c-b9b8-4d7d-a701-cf860462aba4.webp"
     },
     {
