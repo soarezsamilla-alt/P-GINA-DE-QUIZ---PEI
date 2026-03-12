4b31041e1731010d5adb893c183ebcece35aeb47
Samilla Soarez <soarezsamilla@gmail.com>
2026-03-12 21:40:58 +0000

    troque somente esse texto e nada alem disso, troque para esse: Um guia c

--- src/components/quiz/QuizContainer.tsx
+++ src/components/quiz/QuizContainer.tsx
@@ -64,7 +64,7 @@ export const QuizContainer: React.FC = () => {
   const bonuses = [
     {
       title: "Atividades Adaptadas",
-      description: "100 atividades prontas para aplicar hoje.",
+      description: "Um guia completo com apoio individualizado e estratégias de atividades adaptadas.",
       image: "https://image2url.com/r2/default/images/1773351312705-04ec6820-800f-4144-8bd9-5694237c8b28.webp"
     },
     {
