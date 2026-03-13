49eae55f3ef005e4abc94e6f7b0046efd420bccc
Samilla Soarez <soarezsamilla@gmail.com>
2026-03-13 20:36:56 +0000

    troque o texto da etapa 5 😆 Gostaria de ter encontrado isso antes troqu

--- src/components/quiz/QuizContainer.tsx
+++ src/components/quiz/QuizContainer.tsx
@@ -61,7 +61,7 @@ export const QuizContainer: React.FC = () => {
       question: "Qual é o perfil dos seus alunos que precisam de PEI?",
       options: [
         { label: "TEA, TDAH ou Deficiência Intelectual", icon: "🧩" },
-        { label: "Gostaria de ter encontrado isso antes", icon: "😆" },
+        { label: "Deficiências múltiplas ou outros diagnósticos", icon: "🔀" },
         { label: "Educação Infantil 4 e 5 anos", icon: "🌱" }
       ]
     }
