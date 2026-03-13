8dc45503ecb119dddda28220799492488086d9c4
Samilla Soarez <soarezsamilla@gmail.com>
2026-03-13 00:06:01 +0000

    Agora aumente a imagem do card do preço um pouco mais.

--- src/components/product/OfferSection.tsx
+++ src/components/product/OfferSection.tsx
@@ -94,7 +94,7 @@ export const OfferSection: React.FC = () => {
             </div>
             <p className="text-[11px] font-bold text-[#5c6570] uppercase tracking-widest">Pagamento único</p>
             
-            <div className="mt-8 relative w-full h-44">
+            <div className="mt-8 relative w-full h-52">
               <Image 
                 src="https://image2url.com/r2/default/images/1773359501312-a55c79a5-0b66-4551-955d-14a5787c7bb4.webp"
                 alt="Garantia"
