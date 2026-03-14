567c485e4849fa004e38e391cd202a8aaec82c1c
Samilla Soarez <soarezsamilla@gmail.com>
2026-03-14 19:05:44 +0000

    aumente somente esse texto para 19px (_for element p_)

--- src/components/product/PersonalizedDescription.tsx
+++ src/components/product/PersonalizedDescription.tsx
@@ -49,7 +49,7 @@ export const PersonalizedDescription: React.FC<PersonalizedDescriptionProps> = (
         </CardHeader>
         <CardContent className="p-8">
           <div className="prose prose-blue max-w-none text-muted-foreground leading-relaxed">
-            <p className="text-[18px] font-medium text-foreground whitespace-pre-line text-center tracking-tight">
+            <p className="text-[19px] font-medium text-foreground whitespace-pre-line text-center tracking-tight">
               {highlightText(description)}
             </p>
           </div>
