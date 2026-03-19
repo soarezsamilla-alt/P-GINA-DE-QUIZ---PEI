ee22e853906e50753dfd6acda1e258e441ad0338
Samilla Soarez <soarezsamilla@gmail.com>
2026-03-19 17:33:49 +0000

    Adicione essa imagem como a imagem 06 no cassoel de amostra: https://ima

--- src/components/product/SampleGallery.tsx
+++ src/components/product/SampleGallery.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import React from 'react'
@@ -12,7 +13,7 @@ import {
 } from "@/components/ui/carousel"
 
 export const SampleGallery: React.FC = () => {
-  const samples = PlaceHolderImages.filter(img => img.id.startsWith('pei-sample-')).slice(0, 5)
+  const samples = PlaceHolderImages.filter(img => img.id.startsWith('pei-sample-')).slice(0, 6)
 
   return (
     <div className="py-6 px-4 bg-secondary/30 rounded-3xl">
